90a557288b083138d37f0e83d44adf6582fc7846
EpicYoshiMaster <epicyoshim@gmail.com>
2022-11-21 15:13:55 -0600

    getting there

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,12 @@
-// next.config.js
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = '';
-let basePath = '/';
-
-if (isGithubActions) 
-{
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /**
-   * @type {import('next').NextConfig}
-   */
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
 	images: {
-		loader: 'akamai',
-		path: '',
+	  loader: 'akamai',
+	  path: '',
 	},
-	assetPrefix: assetPrefix,
-  	basePath: basePath,
-}
-
-export default nextConfig;
+	assetPrefix: './',
+  };
+  
+  export default nextConfig;
